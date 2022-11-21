800dfbba1be6faecdae8e8700bc017ab2bd09812
Redhead80 <g.utkin80@yandex.ru>
2022-11-21 12:59:10 +0300

    commit

--- mycloud/settings.py
+++ mycloud/settings.py
@@ -117,9 +117,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Europe/Moscow'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
